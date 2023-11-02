4dbf508b4ea334129182d638cfa5ced7e055c6ad
Abrham007 <abrhamaraya111@gmail.com>
2023-11-02 17:21:21 +0300

    add the frontend end Faucet functionalty

--- frontend/components/Faucet.jsx
+++ frontend/components/Faucet.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { token } from "../../src/declarations/token/index.js"
 
 function Faucet() {
 
-  async function handleClick(event) {
+  const [isDisabled, setDisable] = useState(false);
+  const [buttonText, setText] = useState("Gimme gimme");
 
+  async function handleClick(event) {
+    setDisable(true);
+    const result = await token.payOut();
+    setText(result);
   }
 
   return (
@@ -16,8 +23,8 @@ function Faucet() {
       </h2>
       <label>Get your free DAngela tokens here! Claim 10,000 DANG coins to your account.</label>
       <p className="trade-buttons">
-        <button id="btn-payout" onClick={handleClick}>
-          Gimme gimme
+        <button id="btn-payout" onClick={handleClick} disabled={isDisabled}>
+          {buttonText}
         </button>
       </p>
     </div>
